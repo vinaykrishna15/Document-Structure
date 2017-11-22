825a79eec61060dd1c5434313b2b5a3480ac6d46
Kumar <sameer.g.kumar@accenture.com>
2017-11-22 12:40:46 +0100

    shell file added

---
+++ validate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ echo -----
+ echo validating folders
+ cd C:\Users\sameer.g.kumar\Document-Structure\Document\2017-11-06\Software Capacity/V1/
+ echo -----
+ IF EXIST Changelog.docx then;
+  echo Changelog exists
+ FI
+
